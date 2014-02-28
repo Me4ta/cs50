081fedd36356c1802cba620c4a26d6fa71d1df92
Me4ta <me4ta18@gmail.com>
2014-02-28 15:40:02 -0500

    Created the pseudocod and main loops

---
+++ pset4/trainer.c
@@ -0,0 +1,33 @@
+/**It takes one and only one command-
+* line argument: the number of dolphins to be trained
+*  (> 0). Allow the trainer to enter an age for each
+* dolphin (> 0). 
+*  Finally, print out the age of the oldest dolphin.*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int getAge(void);
+int main(int argc, int argv[]){
+	//check argc = 2 and the numDolph = argc > 0;
+	//string ageDolph = 0;
+	//for (i = 0; i < numDolph; i++){
+		
+		//ageDolph[i] = getAge();
+		//}
+	//int maxAge = 0;
+	//for (i = 0; i < numDolph; i++){
+		//if (ageDilph[i] < maxAge){
+		//maxAge = ageDolph;
+		//}
+	//printf("Max dolphin's age is %d\n", maxAge);
+}
+int getAge(void){
+	//ask "Input age of dolphin"
+	//int age = GetInt();
+	// while (age <= 0){
+			//ask - Did he die? You have to input age more then 0 again for this dolphin.
+			//age = GetInt();
+			}
+	//return age;
+}
